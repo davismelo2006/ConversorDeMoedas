2e77b24affa5b379231b27b4552a0440b546c82c
Davi Melo <davisantosmelo2006@gmail.com>
2022-05-31 18:32:10 -0300

    Mostrar resultado

--- Program.cs
+++ Program.cs
@@ -7,12 +7,6 @@ namespace ConversorDeMoedas
         {
             Console.Clear();
             menu();
-
-            var first_currency = int.Parse(Console.ReadLine());
-            if (first_currency >= 1 && first_currency <= 3)
-            {
-                selectSecondCurrency(first_currency);
-            };
         }
         static void menu()
         {
@@ -22,7 +16,14 @@ namespace ConversorDeMoedas
             Console.WriteLine("2 - Dólar");
             Console.WriteLine("3 - Euro");
             Console.WriteLine("0 - Sair");
+
+            var first_currency = int.Parse(Console.ReadLine());
+            if (first_currency >= 1 && first_currency <= 3)
+            {
+                selectSecondCurrency(first_currency);
+            };
         }
+
         static void selectSecondCurrency(int first_currency)
         {
             Console.WriteLine("Para qual moeda?");
@@ -31,7 +32,7 @@ namespace ConversorDeMoedas
             {
                 if (first_currency != second_currency)
                 {
-                    getData(getCurrencyName(first_currency), getCurrencyName(second_currency));
+                    PrintResult(getCurrencyName(first_currency), getCurrencyName(second_currency));
                 }
                 else
                 {
@@ -40,6 +41,7 @@ namespace ConversorDeMoedas
                 }
             }
         }
+
         static string getCurrencyName(int currency)
         {
             var currency_name = "";
@@ -51,7 +53,8 @@ namespace ConversorDeMoedas
             }
             return currency_name;
         }
-        static void getData(string first_currency, string second_currency)
+
+        static double getPrice(string first_currency, string second_currency)
         {
             HttpClient client = new HttpClient();
             var url = $"https://economia.awesomeapi.com.br/last/{first_currency}-{second_currency}";
@@ -61,8 +64,20 @@ namespace ConversorDeMoedas
             // o nome variava conforme a moeda que era convertida, dificultando o acesso pela classe. Dessa forma, substitui o nome que era por exemplo USDBRL, EURBRL etc para DATA
             var contentJSON_geral = contentJSON.Replace(first_currency + second_currency, "DATA");
             var result = JsonConvert.DeserializeObject<root>(contentJSON_geral);
+            var price = double.Parse(result.DATA.bid.Replace(".", ","));
 
-            Console.WriteLine(result.DATA.bid);
+            return price;
         }
+
+        static void PrintResult(string first_currency, string second_currency)
+        {
+            Console.WriteLine("Qual valor?");
+            var value = double.Parse(Console.ReadLine());
+            var price = getPrice(first_currency, second_currency);
+            var result = value * price;
+
+            Console.WriteLine($"O resultado é: {result.ToString("N2")}");
+        }
+
     }
 }
